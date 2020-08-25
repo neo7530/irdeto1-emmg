6f8fc7503cff8ca1c3657aadd55544ddb5d8c024
Marco Wabbel <mw@alogis.com>
2020-08-25 10:06:34 +0200

    Changed to TXT Database format

--- linux_emmg.c
+++ linux_emmg.c
@@ -5,18 +5,27 @@
 #include <unistd.h> 
 #include <string.h>
 #include "parser.h" 
-#define PORT 9999 
    
 
 //     int BytesSent, nlen;
-     int i;
-     int timer;
-     uint8_t frame[255];
-     char tbuffer[33];
-
+int i;
+int timer;
+uint8_t frame[255];
+char tbuffer[33];
+bool fileopen = 0;
+bool fileerr = 0;
+FILE *fp = NULL;
+char temm[200] = {0};
 
 int main(int argc, char const *argv[]) 
 { 
+    unsigned int port;
+    if(argc != 2){
+        fprintf(stderr,"No Port defined! Exiting...\n");
+        return -1;
+    }
+
+    port = atoi(argv[1]);
     readkeys();
     int sock = 0, BytesSent; 
     struct sockaddr_in serv_addr; 
@@ -29,7 +38,7 @@ int main(int argc, char const *argv[])
     } 
    
     serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
+    serv_addr.sin_port = htons(port); 
        
     // Convert IPv4 and IPv6 addresses from text to binary form 
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
@@ -71,51 +80,73 @@ int main(int argc, char const *argv[])
 
 while(true){
 
-     if(timer <= time(0)){
-     ifstream file("database.bin", ios::in|ios::binary|ios::ate);
-      if (file.is_open())
+    if(timer <= time(0)){
+
+//    ifstream file("database.bin", ios::in|ios::binary|ios::ate);
+
+	if((!fileopen) && (!fileerr)){
+		fp = fopen("./emm.txt", "rt");
+		if(fp == NULL){
+			fprintf(stderr,"\n\nemm.txt not found!\n\n");
+			fileerr = 1;
+		} else {
+			fprintf(stderr,"\n\nemm.txt opened\n\n ");
+			//fprintf(stderr, "%s\n\n", ctime(&now));
+			fileopen = 1;
+		}
+	}
+
+
+
+    if (fileopen)
     {
-        file.seekg(0, ios::beg);
-        while(!file.eof()){
-            file.read (tbuffer,33);
+        //file.seekg(0, ios::beg);
+        while(!feof(fp)){
+            fgets(temm,200,fp);
+			//printf(temm);
+			for(int idx = 0;idx < 33;idx++){
+				sscanf(&temm[idx*3],"%02x ",&tbuffer[idx]);
+			}
+					
+			//file.read (tbuffer,33);
             parse(frame,tbuffer);
             printf("EMM to SEND: \n");
             for (int i=0;i <=(frame[2]+2) ;i++){printf("%02X ",frame[i]);}
             printf("\n");
 
-            genemm(sendbuf);
-            memcpy(&sendbuf[43],&frame[0],(frame[2]+3));
-            sendbuf[42] = frame[2]+3;
+            genframe(sendbuf);
+            copyemm(sendbuf,frame);
+
             for(int i =0;i < 0xcf;i++){printf("%02X ",sendbuf[i]&0xff);}
             printf("\n");
             BytesSent = send(sock, sendbuf, ((sendbuf[4]+5) &0xff ), 0);
 
-     if(BytesSent == SO_ERROR)
-          printf("Client: send() error.\n");
-     else
-     {
-          printf("Client: send() is OK - bytes sent: %d\n", BytesSent);
-     }
-     usleep(50000);
+			if(BytesSent == SO_ERROR){
+				printf("Client: send() error.\n");
+			} else {
+				printf("Client: send() is OK - bytes sent: %d\n", BytesSent);
+			}
+			usleep(50000);
         }
-    file.close();
-    printf("Database processed... Continue sending NULL-Packets\n");
-    timer = (time(0)+120);
-    }
-    else printf("ERROR: database.bin not found! \n");
-    timer = (time(0)+120);
+		fclose(fp);
+		fileopen = 0;
+		printf("Database processed... Continue sending NULL-Packets\n");
+		timer = (time(0)+120);
+    } else {
+		printf("ERROR: database.bin not found! \n");
+		timer = (time(0)+120);
+	}
     }
 
 
-	genemm(sendbuf);
+	genframe(sendbuf);
 	BytesSent = send(sock, sendbuf, ((sendbuf[4]+5) &0xff ), 0);
 
-	if(BytesSent == SO_ERROR)
+	if(BytesSent == SO_ERROR){
           printf("Client: send() error .\n");
-	else
-     {
+	} else {
           //printf("Client: send() is OK - bytes sent: %d\n", BytesSent);
-     }
+    }
 
 usleep(50000);
 }
